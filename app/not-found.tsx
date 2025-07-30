4734d91a310d77099d5c67492d4742a2f71f66a6
KoiralaSam <samarpankoirala@gmail.com>
2025-07-29 20:59:18 -0500

    refactoring folder structure

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen"></div>
+  );
+};
+
+export default NotFoundPage;
