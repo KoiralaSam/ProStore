60404c32ee51ed01d9a98e8301f1e260b70048a8
KoiralaSam <samarpankoirala@gmail.com>
2025-08-17 20:13:37 -0500

    added loading and not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,28 @@ import { Button } from "@/components/ui/button";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen"></div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        src="/images/logo.svg"
+        width={48}
+        height={48}
+        alt={`${APP_NAME} logo`}
+        priority={true}
+      />
+      <div className="p-6 w-1/3rounded-lg shadow-md text-center">
+        <h1 className="text-3xl font-bold mb-4">Not Found</h1>
+        <p className="text-destructive">Could not find page</p>
+        <Button
+          variant="outline"
+          className="mt-4"
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        >
+          Back To Home
+        </Button>
+      </div>
+    </div>
   );
 };
 
